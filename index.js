a894525cfd1c792ba043b92f1341dbf5df9e1d1a
T013D0 <josetoledoo6421@gmail.com>
2024-01-17 14:10:59 -0300

    testing

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
-  $("#more").click(function() {
+  $("#more").click(function () {
     $(".downcontent").toggle();
   });
-  $("#discord").click(function() {
-    console.log("a");
-    navigator.clipboard.writeText("kidkuda");
-    alert("Discord copied to clipboard");
-    console.log("holaa");
-  });
 });
+
+function discord() {
+  console.log("a");
+  navigator.clipboard.writeText("kidkuda");
+  alert("Discord copied to clipboard");
+  console.log("holaa");
+}
