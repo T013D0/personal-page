e75bf3a201be84f55caff2af1a563bad1e6893e6
T013D0 <josetoledoo6421@gmail.com>
2024-01-17 14:16:52 -0300

    fiex discord and style button

--- index.js
+++ index.js
@@ -3,10 +3,3 @@ $(document).ready(function () {
     $(".downcontent").toggle();
   });
 });
-
-function discord() {
-  console.log("a");
-  navigator.clipboard.writeText("kidkuda");
-  alert("Discord copied to clipboard");
-  console.log("holaa");
-}
