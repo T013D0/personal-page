8955f6e8487bfccf91e9e67be6473e4f794f72d2
T013D0 <josetoledoo6421@gmail.com>
2024-01-17 14:07:03 -0300

    changed discord a to button and renamed youtube

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-  $("#more").click(function () {
+  $("#more").click(function() {
     $(".downcontent").toggle();
   });
-  $("#discord").click(function () {
+  $("#discord").click(function() {
     console.log("a");
     navigator.clipboard.writeText("kidkuda");
     alert("Discord copied to clipboard");
