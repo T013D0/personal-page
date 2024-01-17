2f6d43784f9b095496d9507b2f51f399ce284ba2
T013D0 <josetoledoo6421@gmail.com>
2024-01-17 13:55:33 -0300

    added discord copied

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ $(document).ready(function () {
   $("#more").click(function () {
     $(".downcontent").toggle();
   });
+  $("#discord").click(function () {
+    navigator.clipboard.writeText("kidkuda");
+    alert("Discord copied to clipboard");
+  });
 });
